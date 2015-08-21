771c09e6353d7d222baac1e89c61b0a46e155a32
James Hiebert <hiebert@uvic.ca>
2015-08-21 16:49:45 -0700

    Started a framework for the backend web API calls

---
+++ ce/api.py
@@ -0,0 +1,87 @@
+import inspect
+
+from json import dumps
+from flask import request
+from werkzeug.wrappers import BaseResponse as Response
+
+def call(request_type):
+
+    try:
+        func = methods[request_type]
+    except KeyError:
+        return Response("Bad Request", status=400)
+
+    # Check that required args are included in the query params
+    required_params = get_required_args(func)
+    provided_params = request.args.keys()
+    if set(required_params).difference(set(provided_params)):
+        return Response("Missing query params", status=400)
+
+    # FIXME: Sanitize input
+    args = [ request.args.get(key) for key in required_params ]
+    return Response(dumps(func(*args)), content_type='application/json')
+
+def stats(id_, time, area, variable):
+    return {
+        'model_id1':
+        {
+            'min': 0.0,
+            'max': 2.0,
+            'mean': 1.0,
+            'median': 1.0,
+            'stdev': 1.0,
+            'units': 'degC'
+        },
+    }
+
+def data(id_, time, area, variable):
+    return {
+        'model_id1':
+        {
+            '2020': 10.0,
+            '2050': 20.0,
+            '2080': 35.0,
+            'units': 'degC'
+        },
+    }
+
+def models():
+    return [ 'model_id{}'.format(i) for i in range(5) ]
+
+def metadata(model_id=None):
+    return {
+        'model_id1':
+        {
+            'institute_id': '<string>',
+            'institution': '<string>',
+            'model_id': '<string>',
+            'model_name': '<string>',
+            'experiment': '<string>',
+            'variables': ['<string:var1>', '<string:var2>'],
+            'ensemble_member': '<string>'
+        },
+        'model_id2':
+        {
+            'institute_id': '<string>',
+            'institution': '<string>',
+            'model_id': '<string>',
+            'model_name': '<string>',
+            'experiment': '<string>',
+            'variables': ['<string:var1>', '<string:var2>'],
+            'ensemble_member': '<string>'
+        },
+    }
+
+methods = {
+    'stats': stats,
+    'data': data,
+    'models': models,
+    'metadata': metadata
+}
+
+# from http://stackoverflow.com/questions/196960/can-you-list-the-keyword-arguments-a-python-function-receives
+def get_required_args(func):
+    args, varargs, varkw, defaults = inspect.getargspec(func)
+    if defaults:
+        args = args[:-len(defaults)]
+    return args   # *args and **kwargs are not required, so ignore them.
--- ce/views.py
+++ ce/views.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, request
+
+import ce.api
 
 def add_routes(app):
 
     @app.route("/")
     def index():
         return render_template("index.html", analytics='UA-20166041-3')
+
+    @app.route("/api/<request_type>")
+    def api_request(*args, **kwargs):
+        return ce.api.call(*args, **kwargs)
