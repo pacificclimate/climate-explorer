883ad18a99ce06851cbb0138aacb0293f091c531
Basil Veerman <bveerman@uvic.ca>
2015-08-26 16:39:44 -0700

    Add BC basemap, dos2unix index.js

--- ce/static/js/components/Map/Map.js
+++ ce/static/js/components/Map/Map.js
@@ -1,54 +1,35 @@
 var React = require("react");
 
-var Map = React.createClass({
+var BCMap = React.createClass({
     componentDidMount: function() {
-        // var crs = L.Proj.CRS.TMS(
-        //     'EPSG:3005',
-        //     '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
-        //     [-1000000, -1000000, 3000000, 3000000],
-        //     { 
-        //         resolutions: [7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625, 122.0703125, 61.03515625, 30.517578125, 15.2587890625, 7.62939453125, 3.814697265625]
-        //     }
-        // );
-
-        // var map = L.map(this.getDOMNode(), {
-        //     crs: crs,
-        //     center: new L.LatLng(55, -125),
-        //     minZoom: 2,
-        //     maxZoom: 10,
-        //     zoom: 2,
-        //     maxBounds: L.latLngBounds([[45, -148], [62, -108]]),
-        //     layers: [
-        //  L.tileLayer(
-        //      'http://{s}.tiles.pacificclimate.org/tilecache/tilecache.py/1.0.0/bc_osm/{z}/{x}/{y}.png', 
-        //      {
-        //      subdomains: 'abc',
-        //      maxZoom: 12,
-        //      noWrap: true,
-        //      attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        //      continuousWorld: false
-        //      }
-        //  )
-        //     ],
-        //     worldCopyJump: false
-        // });
-
-        var map = L.map(this.getDOMNode(), {
-            center: [51.505, -120],
-            zoom: 4,
-            maxBounds: L.latLngBounds([[40, -150], [85, -50]]),
-        });
-        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new L.TileLayer(
-            osmUrl,
+        var crs = new L.Proj.CRS.TMS(
+            'EPSG:3005',
+            '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+            [-1000000, -1000000, 3000000, 3000000],
             {
-                minZoom: 3,
-                maxZoom: 12,
-                attribution: osmAttrib
+                resolutions: [7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625, 122.0703125, 61.03515625, 30.517578125, 15.2587890625, 7.62939453125, 3.814697265625]
             }
         );
-        map.addLayer(osm);
+
+        var map = L.map(this.getDOMNode(), {
+            crs: crs,
+            continuousWorld: false,
+            worldCopyJump: false,
+            center: L.latLng(55, -125),
+            minZoom: 2,
+            maxZoom: 12,
+            zoom: 2,
+            maxBounds: L.latLngBounds([[45, -148], [62, -108]]),
+            layers: [
+                L.tileLayer(
+                'http://{s}.tiles.pacificclimate.org/tilecache/tilecache.py/1.0.0/bc_osm/{z}/{x}/{y}.png',
+                {
+                    subdomains: 'abc',
+                    maxZoom: 12,
+                    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                })
+            ]
+        });
 
         map.on('click', this.onMapClick);
         map.fitWorld();
@@ -68,4 +49,4 @@ var Map = React.createClass({
     }
 });
 
-module.exports = Map;
+module.exports = BCMap;
--- ce/static/js/index.js
+++ ce/static/js/index.js
@@ -1,7 +1,7 @@
 var React = require("react");
-var Map = require("./components/Map/Map");
+var BCMap = require("./components/Map/Map");
 
-React.render(<Map />, document.body);
+React.render(<BCMap />, document.body);
 
 var DynamicSearch = require("./components/DynamicSearch/DynamicSearch");
 var datasets = require("./content/Datasets");
@@ -9,4 +9,4 @@ var datasets = require("./content/Datasets");
 // React.render(
 //   <DynamicSearch items={ datasets } />,
 //   document.getElementById('search')
-// ); 
+// );
