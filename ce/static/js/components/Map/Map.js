e2b75e613abab030b4c8d34c8002d8eb2808442c
Basil Veerman <bveerman@uvic.ca>
2015-08-26 15:54:38 -0700

    Working basemap

--- ce/static/js/components/Map/Map.js
+++ ce/static/js/components/Map/Map.js
@@ -1,20 +1,17 @@
 var React = require("react");
 
-var L = require('leaflet')
-require('proj4leaflet')
-
 var Map = React.createClass({
     componentDidMount: function() {
-        // var crs = c.Proj.CRS.TMS(
+        // var crs = L.Proj.CRS.TMS(
         //     'EPSG:3005',
         //     '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
         //     [-1000000, -1000000, 3000000, 3000000],
         //     { 
-        //  resolutions: [7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625, 122.0703125, 61.03515625, 30.517578125, 15.2587890625, 7.62939453125, 3.814697265625]
+        //         resolutions: [7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625, 122.0703125, 61.03515625, 30.517578125, 15.2587890625, 7.62939453125, 3.814697265625]
         //     }
         // );
 
-        // var map = this.map = L.Map(this.getDOMNode(), {
+        // var map = L.map(this.getDOMNode(), {
         //     crs: crs,
         //     center: new L.LatLng(55, -125),
         //     minZoom: 2,
@@ -37,12 +34,25 @@ var Map = React.createClass({
         // });
 
         var map = L.map(this.getDOMNode(), {
-            center: [51.505, -0.09],
-            zoom: 13
+            center: [51.505, -120],
+            zoom: 4,
+            maxBounds: L.latLngBounds([[40, -150], [85, -50]]),
         });
+        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osm = new L.TileLayer(
+            osmUrl,
+            {
+                minZoom: 3,
+                maxZoom: 12,
+                attribution: osmAttrib
+            }
+        );
+        map.addLayer(osm);
 
         map.on('click', this.onMapClick);
         map.fitWorld();
+
     },
     componentWillUnmount: function() {
         this.map.off('click', this.onMapClick);
@@ -53,7 +63,7 @@ var Map = React.createClass({
     },
     render: function() {
         return (
-            <div className='map'></div>
+            <div id='map'></div>
         );
     }
 });
--- ce/static/js/index.js
+++ ce/static/js/index.js
@@ -1,7 +1,6 @@
 var React = require("react");
 var Map = require("./components/Map/Map");
 
-// React.render(<Map />,document.getElementById('map'));
 React.render(<Map />, document.body);
 
 var DynamicSearch = require("./components/DynamicSearch/DynamicSearch");
