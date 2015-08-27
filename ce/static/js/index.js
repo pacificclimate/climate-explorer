f75bf2f32e864eafdb59423783f5b6949f307a0c
Basil Veerman <bveerman@uvic.ca>
2015-08-27 12:13:18 -0700

    Update BC/Canada Map component locations

--- ce/static/js/index.js
+++ ce/static/js/index.js
@@ -1,8 +1,9 @@
 var React = require("react");
-var BCMap = require("./components/Map/Map").BCMap;
-var CanadaMap = require("./components/Map/Map").CanadaMap;
+var BCMap = require("./components/Map/BCMap").BCMap;
+var CanadaMap = require("./components/Map/CanadaMap").CanadaMap;
 
 React.render(<CanadaMap />, document.body);
+// React.render(<BCMap />, document.body);
 
 var DynamicSearch = require("./components/DynamicSearch/DynamicSearch");
 var datasets = require("./content/Datasets");
