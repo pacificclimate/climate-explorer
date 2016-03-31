a256d2b90f44f3c909ef6d8d8e470ab98b888940
Mike Fischer <mfischer@uvic.ca>
2016-03-31 13:35:44 -0700

    Creates new subsection dealing with drop-down menus. Adds some screenshots.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,12 +15,12 @@
 
 import sys
 import os
-import ce.api
+#import ce.api
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('/storage/home/mfischer/code/climate-explorer-backend'))
 
 # -- General configuration ------------------------------------------------
 
